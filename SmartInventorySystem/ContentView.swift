9099833a8bab73c2943c88ffa742236f79cce78c
shchoholiev-opflo <s.shchoholiev@opflo.com>
2023-12-03 11:52:59 -0500

    Delete SmartInventorySystem directory

---
+++ SmartInventorySystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartInventorySystem
+//
+//  Created by Serhii Shchoholiev on 12/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartInventorySystem/SmartInventorySystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartInventorySystemApp.swift
+//  SmartInventorySystem
+//
+//  Created by Serhii Shchoholiev on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartInventorySystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
