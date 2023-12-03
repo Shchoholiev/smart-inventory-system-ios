78bb23d538876b13481787049523aa413a22d136
shchoholiev <sh.codework@gmail.com>
2023-12-03 13:45:18 -0500

    Added register screen

---
+++ SmartInventorySystem/Models/Identity/RegisterModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterModel.swift
+//  SmartInventorySystem
+//
+//  Created by Serhii Shchoholiev on 12/3/23.
+//
+
+import Foundation
---
+++ SmartInventorySystem/Views/Register.swift
@@ -0,0 +1,8 @@
+//
+//  Register.swift
+//  SmartInventorySystem
+//
+//  Created by Serhii Shchoholiev on 12/3/23.
+//
+
+import Foundation
