c3261cfbf21069b5bfa83bc653896dfbbebe279c
shchoholiev <sh.codework@gmail.com>
2023-12-04 20:04:57 -0500

    Added group page with group editing

---
+++ SmartInventorySystem/Models/Role.swift
@@ -0,0 +1,8 @@
+//
+//  Role.swift
+//  SmartInventorySystem
+//
+//  Created by Serhii Shchoholiev on 12/4/23.
+//
+
+import Foundation
---
+++ SmartInventorySystem/Views/GroupView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupView.swift
+//  SmartInventorySystem
+//
+//  Created by Serhii Shchoholiev on 12/4/23.
+//
+
+import SwiftUI
+
+struct GroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupView()
+}
