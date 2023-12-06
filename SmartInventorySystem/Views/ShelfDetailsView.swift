e6b0a2a1a75d517beaac6d34c48684d2cce0822c
shchoholiev <sh.codework@gmail.com>
2023-12-05 23:05:42 -0500

    Added navigation to Shelf Details View

---
+++ SmartInventorySystem/Views/ShelfDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ShelfDetailsView.swift
+//  SmartInventorySystem
+//
+//  Created by Serhii Shchoholiev on 12/5/23.
+//
+
+import SwiftUI
+
+struct ShelfDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShelfDetailsView()
+}
